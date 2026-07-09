525deb70e81f6be494abb9fa678b59d0c34c94b9
Hesham Seliem <heshamseliem9@gmail.com>
2026-07-09 07:54:43 +0300

    secend commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:event_plan_app_3/utiles/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+// hello secend commit
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
